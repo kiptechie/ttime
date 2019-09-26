03246da0377f9ea0d7e1ec3d774c5906b86cc6b0
Timothy K. Serem <kipkorirtimserem@outlook.com>
2019-09-26 10:14:47 +0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,18 +8,19 @@ import { Insomnia } from '@ionic-native/insomnia/ngx';
 })
 export class HomePage  {
 
-  percent:number = 0;
-  radius:number = 100;
-  fullTime:any = '00:01:30';
-  timer:any = false;
-  progress:any = 0;
-  minutes:number = 1;
-  seconds:any = 30;
+  percent = 0;
+  radius = 100;
+  fullTime: any = '00:01:00';
+  timer: any = false;
+  progress: any = 0;
+  minutes = 1;
+  seconds: any = 30;
 
   elapsed: any = {
-    h:'00', m:'00', s:'00'
-  }
-  overallTimer:any = false;
+    h: '00', m: '00', s: '00'
+  };
+
+  overallTimer: any = false;
 
   constructor(private insomnia: Insomnia) { }
 
@@ -48,44 +49,45 @@ export class HomePage  {
     this.percent = 0;
     this.progress = 0;
 
-    let timeSplit = this.fullTime.split(':');
+    const timeSplit = this.fullTime.split(':');
     this.minutes = timeSplit[1];
     this.seconds = timeSplit[2];
 
     // parseInt allows to be seen as an integer
-    let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+    // tslint:disable-next-line: radix
+    const totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
 
     this.timer = setInterval(() => {
 
-      if (this.percent == this.radius)
+      if (this.percent === this.radius) {
         clearInterval(this.timer);
+      }
 
       this.percent = Math.floor((this.progress / totalSeconds) * 100);
-      this.progress++; 
-    }, 1000)
-    
+      this.progress++;
+    }, 1000);
   }
 
   progressTimer() {
-    let countDownDate = new Date();
+    const countDownDate = new Date();
 
     this.overallTimer = setInterval(() => {
-      let now = new Date().getTime();
-      let distance = now - countDownDate.getTime();
+      const now = new Date().getTime();
+      const distance = now - countDownDate.getTime();
 
-    this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
-    this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
 
-    this.elapsed.h = this.pad(this.elapsed.h, 2);
-    this.elapsed.m = this.pad(this.elapsed.m, 2);
-    this.elapsed.s = this.pad(this.elapsed.s, 2);
-    }, 1000)
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+    }, 1000);
   }
 
   pad(num, size) {
-    let s = num+"";
-    while (s.length < size) s = "0" + s;
+    let s = num + '';
+    while (s.length < size) { s = '0' + s; }
     return s;
   }
 
@@ -97,8 +99,8 @@ export class HomePage  {
     this.percent = 0;
     this.progress = 0;
     this.elapsed = {
-      h:'00', m:'00', s:'00'
-    }
+      h: '00', m: '00', s: '00'
+    };
 
     // disable insomnia
     this.insomnia.allowSleepAgain()
